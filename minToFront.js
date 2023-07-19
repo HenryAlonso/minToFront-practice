e5438fde455dafa424f00bcc089d11488bf014dc
Henry Alonso <henryalonso78@gmail.com>
2023-07-19 18:43:03 -0400

    Add files via upload

---
+++ minToFront.js
@@ -0,0 +1,19 @@
+// Given an array of comparable values, move the lowest element to array’s front, shifting backward any elements previously ahead of it.
+// Do not otherwise change the array’s order. Given [4,2,1,3,5], change it to [1,4,2,3,5] and return it. As always, do this without using built-in functions.
+function minToFront(array) {
+    var minIndex = 0;
+    for (var i = 1; i < array.length; i++) {
+        if(array[i] < array[minIndex]) {
+            minIndex = i;
+        }
+    }
+    for (var j = minIndex; j > 0; j--) {
+        var temp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = temp;
+    }
+    return array;
+}
+
+var test10 = minToFront([45, 28, 115, 3, 51])
+console.log(test10);
